00fba446b64f9307d017e0a7f54eb35b45711e48
Odin-son <song921216@gmail.com>
2021-05-19 15:48:50 +0900

    implement validate mask

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import sys
 import os
+import cv2
 
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
+from glob import glob
 
 form_class = uic.loadUiType("main.ui")[0]
 
@@ -11,9 +13,17 @@ class MyWindow(QMainWindow, form_class):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        self.setWindowTitle('mask validator')
         self.import_btn.clicked.connect(self.import_clicked)
-        self.export_btn.clicked.connect(self.export_clicked)
-        self.buttonBox.clicked.connect(self.validate)
+
+        self.color_map = [(0, 0, 0),
+                          (0, 0, 255),
+                          (0, 255, 255),
+                          (255, 0, 255),
+                          (0, 150, 255),
+                          (255, 0, 0),
+                          (0, 255, 0),
+                          (255, 255, 0)]
 
     def import_clicked(self):
         import_dir_path = str(
@@ -26,27 +36,39 @@ class MyWindow(QMainWindow, form_class):
             )
         )
         self.label.setText(import_dir_path)
-
-    def export_clicked(self):
-        export_dir_path = str(
-            QFileDialog.getExistingDirectory(
-                self,
-                self.tr("Open Directory"),
-                os.getcwd(),
-                QFileDialog.ShowDirsOnly
-                | QFileDialog.DontResolveSymlinks,
-            )
-        )
-        self.label_2.setText(export_dir_path)
+        self.validate()
 
     def validate(self):
         if not os.path.exists(self.label.text()):
-            QMessageBox.warning(self, self.tr("Attention"), "import path does not exist", QMessageBox.Yes)
+            QMessageBox.warning(self, self.tr("Attention"), "Directory does not exist", QMessageBox.Yes)
             return
 
-        if not os.path.exists(self.label_2.text()):
-            QMessageBox.warning(self, self.tr("Attention"), "export path does not exist", QMessageBox.Yes)
-            return
+        imgs_path = glob(os.path.join(self.label.text(), '*{}'.format('.png')))
+
+        have_problem = False
+        for img_path in imgs_path:
+            img = cv2.imread(img_path)
+
+            result = img.copy()
+            for idx in range(1, len(self.color_map)):
+                extracted_mask = (img[:, :, 0] == self.color_map[idx][0]) & (img[:, :, 1] == self.color_map[idx][1]) & (
+                        img[:, :, 2] == self.color_map[idx][2])
+                result[extracted_mask] = 0
+
+            if result.max() > 0:
+                have_problem = True
+                result = cv2.cvtColor(result, cv2.COLOR_BGR2GRAY)
+                result[result > 0] = 255
+
+                out_path = os.path.join(os.path.split(img_path)[0],
+                                        '{}_problem.png'.format(os.path.basename(img_path).split('.')[0]))
+
+                cv2.imwrite(out_path, result)
+
+        if have_problem:
+            QMessageBox.warning(self, self.tr("Attention"), "There are some problem", QMessageBox.Yes)
+        else:
+            QMessageBox.warning(self, self.tr("Attention"), "Checked", QMessageBox.Yes)
 
 
 app = QApplication(sys.argv)
